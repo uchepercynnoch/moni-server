9e549123b181b59fc430663ddacb3f462db5f7cd
Edosa Kelvin <edosakelvin@gmail.com>
2019-10-31 12:47:27 +0100

    Refactor! 🦄

--- src/helpers/dinero-helper.js
+++ src/helpers/dinero-helper.js
@@ -10,10 +10,7 @@ const DineroSchema = new mongoose.Schema({
 
 const toDinero = (value, padWithPrecisionZeros = false) => {
     /* Pad while respecting the precsison */
-    const pad = (x, p) => {
-        ;``
-        return Number.parseInt(String(x) + _.repeat('0', p))
-    }
+    const pad = (x, p) => Number.parseInt(String(x) + _.repeat('0', p))
 
     let opts = { amount: 0, currency: 'NGN', precision: 2 }
 
